b771cc1eecebf6e98934d1796b832c44ba1c82f0
orhanyokus <68279206+orhanyokus@users.noreply.github.com>
2023-08-24 10:09:18 +0300

    Add files via upload

---
+++ strmlit.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+
+# Path to the Excel file
+excel_file_path = r'C:\Users\ayefr\OneDrive\Masaüstü\python\STOK.xlsx'
+
+# Load the Excel data
+def load_excel_data():
+    try:
+        data = pd.read_excel(excel_file_path)
+        return data
+    except Exception as e:
+        st.error(f"Error loading data: {e}")
+        return None
+
+def main():
+    st.title("Excel Data Viewer")
+
+    # Load Excel data
+    data = load_excel_data()
+
+    if data is not None:
+        st.sidebar.header("Search")
+
+        # Search bar
+        search_term = st.sidebar.text_input("Enter search term:")
+        filtered_data = data[data.apply(lambda row: search_term.lower() in str(row).lower(), axis=1)]
+
+        st.write("### Filtered Excel Data:")
+        st.dataframe(filtered_data)
+
+if __name__ == "__main__":
+    main()
